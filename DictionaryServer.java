2c64655478ce2da1ead0f56306d7b35562fb3042
danielzl93 <30852195+danielzl93@users.noreply.github.com>
2017-09-12 18:53:23 +1000

    Update DictionaryServer.java

--- DictionaryServer.java
+++ DictionaryServer.java
@@ -1,8 +1,6 @@
 /********A Simple Dictionary with server-client architecture
  *          that supports multiple clients access
- * Author:      Liang Zhang
- * Student ID:  692353
- *
+ * Author:      Daniel Zhang
  */
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
